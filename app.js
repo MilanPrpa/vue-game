44e28a79109e06029d9332d0b4c03fc9cd5124bf
MilanPrpa <milanprpa25@gmail.com>
2018-02-16 11:56:18 -0700

    refactor

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
+
 new Vue({
-	el: '#app',
+	el: "#app",
 	data: {
 		playerHealth: 100,
 		monsterHealth: 100,
 		gameIsRunning: false
 	},
-
 	methods: {
 		startGame: function() {
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
 		},
-		damageCalc: function(min, max) {
-			return Math.max(Math.floor(Math.random() * max) + 1, min)
+		damage: function(min, max) {
+			return Math.max(Math.floor(Math.random() * max) + 1, min);
 		},
 		attack: function() {
-			this.monsterHealth -= this.damageCalc(3, 10)
-			if (this.checkWin()) {
-				return;
-			}
+			this.monsterHealth -= this.damage(5, 8)
+				if (this.checkWin()) {
+					return;
+				}
 
-			this.playerHealth -= this.damageCalc(5, 12);
-			this.checkWin();
+			this.playerHealth -= this.damage(7, 10)
+				 this.checkWin();
 		},
 		special: function() {
-			this.monsterHealth -= this.damageCalc(10,20);
-			if (this.checkWin()) {
-				return;
-			}
-			this.playerHealth -= this.damageCalc(5, 12);
-			this.checkWin();
+
 		},
 		heal: function() {
-			this.playerHealth += this.damageCalc(6, 15)
+
 		},
 		quit: function() {
 
@@ -58,6 +53,96 @@ new Vue({
 		}
 	}
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		playerHealth: 100,
+// 		monsterHealth: 100,
+// 		gameIsRunning: false
+// 	},
+
+// 	methods: {
+// 		startGame: function() {
+// 			this.gameIsRunning = true;
+// 			this.playerHealth = 100;
+// 			this.monsterHealth = 100;
+// 		},
+// 		damageCalc: function(min, max) {
+// 			return Math.max(Math.floor(Math.random() * max) + 1, min)
+// 		},
+// 		attack: function() {
+// 			this.monsterHealth -= this.damageCalc(3, 10)
+// 			if (this.checkWin()) {
+// 				return;
+// 			}
+
+// 			this.playerHealth -= this.damageCalc(5, 12);
+// 			this.checkWin();
+// 		},
+// 		special: function() {
+// 			this.monsterHealth -= this.damageCalc(10,20);
+// 			if (this.checkWin()) {
+// 				return;
+// 			}
+// 			this.playerHealth -= this.damageCalc(5, 12);
+// 			this.checkWin();
+// 		},
+// 		heal: function() {
+// 			this.playerHealth += this.damageCalc(6, 15)
+// 		},
+// 		quit: function() {
+
+// 		},
+// 		checkWin: function() {
+// 			if (this.monsterHealth <= 0) {
+// 				if (confirm('you won, new game?')) {
+// 					this.startGame()
+// 				} else {
+// 					this.gameIsRunning = false;
+// 				}
+// 				return true;
+// 			} else if (this.playerHealth <= 0) {
+// 				if (confirm('you lost, new game?')) {
+// 					this.startGame()
+// 				} else {
+// 					this.gameIsRunning = false;
+// 				} 
+// 				return true;
+// 			}
+// 				return false;
+// 		}
+// 	}
+// })
 
 
 
